08dfcb284300000e383d3169a60d4ded3721292a
Alexander <pimalex1978@yandex.ru>
2020-08-19 19:30:45 +0300

    add file functions.js

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function isPalindrome(numberAsString){
+    let characters = numberAsString.split('');
+    let isPalindrome = true;
+
+    for (let index = 0; index < characters.length; index++){
+        const element = characters[index];
+
+        if (element !== characters[characters.length - 1 - index]) {
+            isPalindrome = false;
+        }
+    }
+    return isPalindrome;
+}
+
+// console.log(isPalindrome("123")); //false
+console.log(isPalindrome("111")); //true
